311dd39e455da4822edfa3226d7265b34847ab0c
Tyler Ward <tyler.t.ward@icloud.com>
2022-06-26 12:32:04 -0500

    fix email styles on contact page

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,7 +1,6 @@
 <div class="main">
 	<h1>Get In Touch</h1>
-	<p>Send an email to <code>tyler@tracerbuilt.dev</code></p>
-	<p>I'd be glad to hear from you</p>
+	<p><code>tyler@tracerbuilt.dev</code></p>
 </div>
 
 <style lang="scss">
@@ -12,5 +11,14 @@
 		height: calc(100vh - 8rem - var(--space--4));
 		justify-content: center;
 		align-items: center;
+	}
+
+	p {
+		margin-top: var(--space-0);
+	}
+
+	code {
+		background: none;
+		font-size: var(--step-1);
 	}
 </style>
