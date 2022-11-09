4886a11cf64cbac03f050cc231bc5ca4f2b86331
Tyler Ward <tyler.t.ward@icloud.com>
2022-11-09 14:12:48 -0600

    maybe this will fix it

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -3,7 +3,7 @@
 	const routes = [
 		{ path: '/#work', text: 'Work' },
 		{ path: '/#about', text: 'About' },
-		{ path: '/#contact', text: 'Contact' }
+		{ path: '/#get_in_touch', text: 'Contact' }
 	]
 	const links = [
 		{ href: 'https://github.com/TracerBuilt', src: '/icons/github.svg', alt: 'Github' },
