02a12d3fc7d3930d6bb9fa472a3dcb1dfdf18a28
Tyler Ward <tyler.t.ward@icloud.com>
2022-11-09 14:28:39 -0600

    fix drop shadows

--- src/lib/components/Photo_Border.svelte
+++ src/lib/components/Photo_Border.svelte
@@ -5,8 +5,8 @@
 <div
 	class={`p-0.5 bg-gradient-to-br from-blue-9 via-purple-9 to-red-9 [&>picture>img]:rounded-md rounded-lg ${
 		mobile
-			? 'absolute -bottom-[2rem] right-0 w-1/5 shadow-xl shadow-grey-7'
-			: 'w-11/12 mx-auto shadow-grey-9 shadow-lg'
+			? 'absolute -bottom-[2rem] right-0 w-1/5 shadow-xl shadow-grey-7 dark:shadow-grey-12'
+			: 'w-11/12 mx-auto shadow-grey-9 dark:shadow-grey-12 shadow-lg'
 	}`}
 >
 	<slot />
