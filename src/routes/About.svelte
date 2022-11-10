22d7f828747d76ab8492803cd3a7d8f993087835
Tyler Ward <tyler.t.ward@icloud.com>
2022-11-10 13:07:18 -0600

    change about spacing and image

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -7,8 +7,8 @@
 	in:fly={{ duration: 400, x: -200, easing: cubicInOut }}
 	class="text-center dark:text-grey-3 text-grey-1 w-screen bg-gradient-to-br from-blue-9 via-purple-9 pb-12 pt-4 -skew-y-3 to-red-9 -mx-2 mb-16 dark:from-blue-11 dark:via-purple-11 dark:to-red-11"
 >
-	<div class="w-11/12 skew-y-3 mx-auto">
-		<h2 in:fly={{ duration: 400, x: -200, easing: cubicInOut, delay: 200 }} class="text-3xl">
+	<div class="max-w-4xl skew-y-3 mx-auto">
+		<h2 in:fly={{ duration: 400, x: -200, easing: cubicInOut, delay: 200 }} class="text-3xl mb-4">
 			About Me
 		</h2>
 		<div class="flex flex-col items-center justify-center md:flex-row md:items-start">
@@ -28,15 +28,16 @@
 			</div>
 			<picture
 				in:scale={{ duration: 400, easing: expoInOut, delay: 1000 }}
-				class="flex w-1/4 aspect-square flex-shrink-0 items-center justify-center border-1 border-grey-0/25 bg-grey-3/50 dark:bg-grey-9/25 backdrop-blur-sm rounded-full shadow-md shadow-grey-9 dark:shadow-grey-12"
+				class="flex w-1/3 mt-8 md:mt-0 md:w-1/4 max-w-sm aspect-square flex-shrink-0 items-center justify-center border-1 border-grey-0/25 bg-grey-3/50 dark:bg-grey-9/25 backdrop-blur-sm rounded-full shadow-sm shadow-grey-9/50 dark:shadow-grey-12/50"
 			>
-				<source srcset="/images/profile-picture.webp" type="image/webp" />
-				<source srcset="/images/profile-picture.jpg" type="image/jpeg" />
+				<source srcset="/images/headshot.avif" type="image/avif" />
+				<source srcset="/images/headshot.webp" type="image/webp" />
+				<source srcset="/images/headshot.jpg" type="image/jpeg" />
 				<img
-					src="/images/profile-picture.jpg"
-					alt="Me, in a painted style, with a moustache. I look pretty good."
-					width="1031"
-					height="1031"
+					src="/images/headshot.jpg"
+					alt="Me, smiling, with a sunset over the badlands blurred in the background. I look pretty good."
+					width="2048"
+					height="2048"
 					class="w-[93%] h-auto rounded-full"
 				/>
 			</picture>
